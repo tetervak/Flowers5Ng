ac1b368164c7707dde85831d5cea18a275c702e4
Alex Tetervak <tetervak@sheridancollege.ca>
2019-11-30 15:01:37 -0500

    copied Flowers4Ng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Flower} from './flower';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Flowers5Ng';
+  // to initialize the flowers
+  private static names: string[] = ['aster', 'carnation', 'daffodil', 'orchid', 'rose'];
+  // all flower objects
+  public flowerList: Flower[];
+  // the selected flower object
+  public selectedFlower: Flower;
+  // noinspection JSMismatchedCollectionQueryUpdate
+  private images: HTMLImageElement[];
+  public title = 'Flowers 4 Ng';
+  constructor() {
+    this.flowerList = [];
+    this.images = [];
+    this.makeFlowers();
+    this.preloadImages();
+    this.selectFlower(this.flowerList[0]);
+  }
+  // make flower objects
+  private makeFlowers() {
+    AppComponent.names.forEach(name => {
+      this.flowerList.push(new Flower(name));
+    });
+  }
+  // preload the large images from the server
+  private preloadImages() {
+    this.flowerList.forEach(flower => {
+      const image: HTMLImageElement = new Image();
+      image.src = flower.largeImgSrc;
+      this.images.push(image);
+    });
+  }
+  // show the flower by the index
+  public selectFlower(flower: Flower) {
+    this.selectedFlower = flower;
+  }
 }
+
